3a17c92b254f635f61b3386a700bddbfbd64077d
Samuel Bronson <naesten@gmail.com>
2009-04-19 17:13:45 -0400

    Use commas instead of redirecting input to BC and LINK from files.

--- xcom.bat
+++ xcom.bat
@@ -1,16 +1,15 @@
 COPY SIM\X86.DAT X86.DAT < UTIL\NIL2
 COPY SIM\X86.BI X86.BI < UTIL\NIL2
-bc /e/x/o/t/c:512 sim\x86.bas < util\nil2 
-bc /e/x/o/t/c:512 sim\keyhelp.bas < kh.nil 
-bc /e/x/o/t/c:512 sim\comutil.bas < cu.nil
-bc /e/x/o/t/c:512 sim\alu.bas < util\nil2 
-bc /e/x/o/t/c:512 sim\parse.bas < util\nil2 
-bc /e/x/o/t/c:512 sim\execute.bas < xx.nil 
-bc /e/x/o/t/c:512 sim\exec0f.bas < 0f.nil 
-bc /e/x/o/t/c:512 sim\segments.bas < sg.nil 
-bc /e/x/o/t/c:512 sim\doto.bas < util\nil2 
-bc /e/x/o/t/c:512 sim\quasi.bas < util\nil2 
-bc /e/x/o/t/c:512 sim\trap.bas < util\nil2 
-bc /e/x/o/t/c:512 stufstub.bas < st.nil
-link x86.obj st.obj kh.obj cu.obj alu.obj parse.obj quasi.obj xx.obj 0f.obj sg.obj trap.obj doto.obj < link.in
-
+bc /e/x/o/t/c:512 sim\x86.bas;
+bc /e/x/o/t/c:512 sim\keyhelp.bas,kh.obj;
+bc /e/x/o/t/c:512 sim\comutil.bas,cu.obj;
+bc /e/x/o/t/c:512 sim\alu.bas;
+bc /e/x/o/t/c:512 sim\parse.bas;
+bc /e/x/o/t/c:512 sim\execute.bas,xx.obj;
+bc /e/x/o/t/c:512 sim\exec0f.bas,0f.obj;
+bc /e/x/o/t/c:512 sim\segments.bas,sg.obj;
+bc /e/x/o/t/c:512 sim\doto.bas;
+bc /e/x/o/t/c:512 sim\quasi.bas;
+bc /e/x/o/t/c:512 sim\trap.bas;
+bc /e/x/o/t/c:512 stufstub.bas,st.obj;
+link x86.obj+st.obj+kh.obj+cu.obj+alu.obj+parse.obj+quasi.obj+xx.obj+0f.obj+sg.obj+trap.obj+doto.obj,edl.exe,,qb.lib;
